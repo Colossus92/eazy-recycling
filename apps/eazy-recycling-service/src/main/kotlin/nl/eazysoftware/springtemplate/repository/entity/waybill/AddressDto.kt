3bdc55f03d4d39b3be46b37562a8a9c801560559
Calvin <info@crsoftware.engineering>
2025-02-14 17:30:43 +0100

    Setup with supabase

--- apps/eazy-recycling-service/src/main/kotlin/nl/eazysoftware/springtemplate/repository/entity/waybill/AddressDto.kt
+++ apps/eazy-recycling-service/src/main/kotlin/nl/eazysoftware/springtemplate/repository/entity/waybill/AddressDto.kt
@@ -1,12 +1,10 @@
 package nl.eazysoftware.springtemplate.repository.entity.waybill
 
 import jakarta.persistence.Embeddable
-import jakarta.persistence.Table
 import lombok.NoArgsConstructor
 
 @Embeddable
 @NoArgsConstructor
-@Table(name = "addresses")
 data class AddressDto(
     var streetName: String?,
     var buildingName: String?,
