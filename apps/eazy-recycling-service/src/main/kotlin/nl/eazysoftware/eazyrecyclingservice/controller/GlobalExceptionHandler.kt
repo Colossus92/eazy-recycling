7878988ca43be5da70d7d714e73a96304f641de9
Calvin <info@crsoftware.engineering>
2025-03-18 19:23:22 +0100

    Add error logging to GlobalExceptionHandler

--- apps/eazy-recycling-service/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/controller/GlobalExceptionHandler.kt
+++ apps/eazy-recycling-service/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/controller/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package nl.eazysoftware.eazyrecyclingservice.controller
 
 import jakarta.persistence.EntityNotFoundException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
 
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
     @ExceptionHandler(Exception::class)
     fun handleException(ex: Exception): ResponseEntity<Map<String, String>> {
+        log.error("An error occurred", ex)
         val errorResponse = mapOf("message" to (ex.message ?: "An error occurred"))
 
         return ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR)
@@ -18,6 +22,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleEntityNotFoundException(ex: EntityNotFoundException): ResponseEntity<Map<String, String>> {
+        log.error("Entity could not be found:", ex)
         val errorResponse = mapOf("message" to (ex.message ?: "An error occurred"))
 
         return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
