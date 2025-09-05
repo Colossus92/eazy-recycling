5fad8ff500e8476ae1f2ae658534e3c0970eadb2
Calvin <info@crsoftware.engineering>
2025-09-05 14:46:59 +0200

    Move .do folder to root of project

--- apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/config/web/RequestLoggingFilter.kt
+++ apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/config/web/RequestLoggingFilter.kt
@@ -25,8 +25,8 @@ class RequestLoggingFilter : Filter {
         val queryString = httpRequest.queryString ?: ""
         val headers = httpRequest.headerNames.toList().joinToString { "$it=${httpRequest.getHeader(it)}" }
 
-        logger.info("Incoming Request: Method=$method, URI=$uri, Query=$queryString, Body=$httpRequest.Headers=[$headers]")
-        
+        logger.info("Incoming Request: Method=$method, URI=$uri, Query=$queryString, Body=$httpRequest Headers=[$headers]")
+
         chain.doFilter(request, response)
 
         val status = httpResponse.status
