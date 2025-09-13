e7d0f2960a8796902c408adb150dac96870a734f
Calvin <info@crsoftware.engineering>
2025-09-13 15:30:46 +0200

    Delete AssignWaybillTransportRequest.kt

--- apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/controller/transport/AssignWaybillTransportRequest.kt
+++
@@ -1,9 +0,0 @@
-package nl.eazysoftware.eazyrecyclingservice.controller.transport
-
-import java.util.UUID
-
-data class AssignWaybillTransportRequest(
-    val licensePlate: String,
-    val waybillId: UUID,
-    val driverId: UUID,
-)
