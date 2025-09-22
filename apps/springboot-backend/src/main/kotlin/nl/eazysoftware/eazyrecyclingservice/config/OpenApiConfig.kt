9e9209c80552d96fd761f60886766874c96c50a9
Calvin <info@crsoftware.engineering>
2025-09-22 20:28:14 +0200

    fix: update jwt-issuer env var name in application config

---
+++ apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/config/OpenApiConfig.kt
@@ -0,0 +1,32 @@
+package nl.eazysoftware.eazyrecyclingservice.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val securitySchemeName = "bearerAuth"
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization")
+
+        val securityRequirement = SecurityRequirement().addList(securitySchemeName)
+
+        return OpenAPI()
+            .components(
+                Components()
+                    .addSecuritySchemes(securitySchemeName, securityScheme)
+            )
+            .addSecurityItem(securityRequirement)
+    }
+}
