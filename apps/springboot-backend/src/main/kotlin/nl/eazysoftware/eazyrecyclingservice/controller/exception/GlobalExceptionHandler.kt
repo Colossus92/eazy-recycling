0819e7c9f10ea06aebf37d8fff88f3c5a5608f44
Calvin <info@crsoftware.engineering>
2025-10-13 15:01:59 +0200

    Log exception in exception handler

--- apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/controller/exception/GlobalExceptionHandler.kt
+++ apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/controller/exception/GlobalExceptionHandler.kt
@@ -1,6 +1,8 @@
 package nl.eazysoftware.eazyrecyclingservice.controller.exception
 
 import jakarta.persistence.EntityNotFoundException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.dao.DuplicateKeyException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,40 +12,57 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(DuplicateKeyException::class)
-    fun handleDuplicateKeyException(ex: DuplicateKeyException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            message = ex.message ?: "Object met id bestaat al",
-        )
-        return ResponseEntity(errorResponse, HttpStatus.CONFLICT)
-    }
+  private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
-    @ExceptionHandler(EntityNotFoundException::class)
-    fun handleEntityNotFoundException(ex: EntityNotFoundException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            message = ex.message ?: "Object niet gevonden",
-        )
-        return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
-    }
-
-    @ExceptionHandler(IllegalArgumentException::class)
-    fun handleDuplicateKeyException(ex: IllegalArgumentException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            message = ex.message ?: "Ongeldige invoer",
-        )
-        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
-    }
-
-
-    @ExceptionHandler(IllegalStateException::class)
-    fun handleDuplicateKeyException(ex: IllegalStateException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            message = ex.message ?: "Deze actie brengt de data in een ongeldige toestand en is niet toegestaan",
-        )
-        return ResponseEntity(errorResponse, HttpStatus.CONFLICT)
-    }
-
-    data class ErrorResponse(
-        val message: String,
+  @ExceptionHandler(DuplicateKeyException::class)
+  fun handleDuplicateKeyException(ex: DuplicateKeyException): ResponseEntity<ErrorResponse> {
+    val errorResponse = ErrorResponse(
+      message = ex.message ?: "Object met id bestaat al",
     )
+
+    logException(ex)
+
+    return ResponseEntity(errorResponse, HttpStatus.CONFLICT)
+  }
+
+  @ExceptionHandler(EntityNotFoundException::class)
+  fun handleEntityNotFoundException(ex: EntityNotFoundException): ResponseEntity<ErrorResponse> {
+    val errorResponse = ErrorResponse(
+      message = ex.message ?: "Object niet gevonden",
+    )
+    logException(ex)
+
+    return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
+  }
+
+  @ExceptionHandler(IllegalArgumentException::class)
+  fun handleDuplicateKeyException(ex: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+    val errorResponse = ErrorResponse(
+      message = ex.message ?: "Ongeldige invoer",
+    )
+
+    logException(ex)
+
+    return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+  }
+
+
+  @ExceptionHandler(IllegalStateException::class)
+  fun handleDuplicateKeyException(ex: IllegalStateException): ResponseEntity<ErrorResponse> {
+    val errorResponse = ErrorResponse(
+      message = ex.message ?: "Deze actie brengt de data in een ongeldige staat en is niet toegestaan",
+    )
+
+    logException(ex)
+
+    return ResponseEntity(errorResponse, HttpStatus.CONFLICT)
+  }
+
+  private fun logException(ex: Exception) {
+    log.error("Exception caught by GlobalExceptionHandler: ${ex.javaClass.simpleName}", ex)
+  }
+
+  data class ErrorResponse(
+    val message: String,
+  )
 }
