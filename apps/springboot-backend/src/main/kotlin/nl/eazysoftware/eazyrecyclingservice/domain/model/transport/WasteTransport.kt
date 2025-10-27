615e6b49feb19138f67db3a154dd117b6c920cd0
Calvin <info@crsoftware.engineering>
2025-10-27 06:17:24 +0100

    Remove not required fields from WasteTransport.kt

--- apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/domain/model/transport/WasteTransport.kt
+++ apps/springboot-backend/src/main/kotlin/nl/eazysoftware/eazyrecyclingservice/domain/model/transport/WasteTransport.kt
@@ -11,53 +11,44 @@ import kotlin.time.Duration
 
 class WasteTransport(
   val transportId: TransportId? = null, // TODO remove UUID generation in DTO
-  
+
   /**
    * Used for human-readable display in the UI
    */
   val displayNumber: TransportDisplayNumber? = null, // TODO get value from database in domain service
   
   /**
-   * The party client ordering the transport.
-   */
-  val consignorParty: CompanyId,
-  
-  /**
    * The party executing the transport.
    */
   val carrierParty: CompanyId,
-  
-  val pickupLocation: Location,
-  
+
   val pickupDateTime: Instant,
-  
-  val deliveryLocation: Location,
-  
+
   val deliveryDateTime: Instant?,
-  
+
   val transportType: TransportType,
-  
+
   val wasteStreamNumber: WasteStreamNumber,
-  
+
   val wasteContainer: WasteContainerId?,
-  
+
   override val truck: LicensePlate?,
-  
+
   override val driver: UserId?,
-  
+
   val note: Note,
-  
+
   override val transportHours: Duration?,
-  
+
   val updatedAt: Instant,
-  
+
   /**
    * Used for ordering transports within the planning
    */
   val sequenceNumber: Int,
-  
+
 ) : Transport {
-  
+
   /**
    * Get the current status of this transport.
    * Delegates to the domain service for status calculation.
