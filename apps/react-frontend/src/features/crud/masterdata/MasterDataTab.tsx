48f476cd6422d01d8261903245fbf568cbedc0b9
Calvin <info@crsoftware.engineering>
2025-11-20 15:16:12 +0100

    When search, reset page

--- apps/react-frontend/src/features/crud/masterdata/MasterDataTab.tsx
+++ apps/react-frontend/src/features/crud/masterdata/MasterDataTab.tsx
@@ -11,120 +11,146 @@ import { ClipLoader } from 'react-spinners';
 import { ErrorThrowingComponent } from '@/components/ErrorThrowingComponent';
 
 export type Column<T> = {
-    key: keyof T;
-    label: string;
-    accessor: (value: T) => string | undefined;
-    width: string;
+  key: keyof T;
+  label: string;
+  accessor: (value: T) => string | undefined;
+  width: string;
 };
 
 export interface DataTableProps<T> {
-    items: T[];
-    columns: Column<T>[];
+  items: T[];
+  columns: Column<T>[];
 }
 
 export interface MasterDataTabProps<T> {
-    data: DataTableProps<T>;
-    searchQuery: (query: string) => void;
-    openAddForm: () => void;
-    editAction: (item: T) => void;
-    removeAction: (item: T) => void;
-    renderEmptyState: (onClick: () => void) => ReactNode;
-    isLoading: boolean;
-    errorHandling: {
-        error: Error | null;
-        reset: () => void;
-    };
+  data: DataTableProps<T>;
+  searchQuery: (query: string) => void;
+  openAddForm: () => void;
+  editAction: (item: T) => void;
+  removeAction: (item: T) => void;
+  renderEmptyState: (onClick: () => void) => ReactNode;
+  isLoading: boolean;
+  errorHandling: {
+    error: Error | null;
+    reset: () => void;
+  };
 }
 
-export const MasterDataTab = <T,>({ data, searchQuery, openAddForm, editAction, removeAction, renderEmptyState, isLoading, errorHandling }: MasterDataTabProps<T>) => {
-    const [page, setPage] = useState(1);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
+export const MasterDataTab = <T,>({
+  data,
+  searchQuery,
+  openAddForm,
+  editAction,
+  removeAction,
+  renderEmptyState,
+  isLoading,
+  errorHandling,
+}: MasterDataTabProps<T>) => {
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    const setQueryAndResetPage = (query: string) => {
-        searchQuery(query);
-        setPage(1);
-    }
+  const setQueryAndResetPage = (query: string) => {
+    searchQuery(query);
+    setPage(1);
+  };
 
-    return (
-        <TabPanel className={"flex-1 flex flex-col items-start self-stretch gap-4 overflow-hidden"}>
-            <ContentTitleBar setQuery={setQueryAndResetPage}>
-                <Button
-                    variant={'primary'}
-                    icon={Plus}
-                    label={'Voeg toe'}
-                    onClick={openAddForm}
-                />
-            </ContentTitleBar>
-            <ErrorBoundary fallbackRender={fallbackRender} onReset={errorHandling.reset}>
-                <ErrorThrowingComponent error={errorHandling.error} />
-                {data.items.length === 0 ? (
-                    isLoading ? (
-                        <div className="flex justify-center items-center h-24 w-full">
-                            <ClipLoader
-                                size={20}
-                                color={'text-color-text-invert-primary'}
-                                aria-label="Laad spinner"
-                            />
-                        </div>
-                    ) : renderEmptyState ? (
-                        renderEmptyState(openAddForm)
-                    ) : null
-                ) : (
-                    <div className="flex-1 items-start self-stretch border-t-solid border-t border-t-color-border-primary overflow-y-auto">
-                        <table className="w-full table-fixed border-collapse">
-                            <colgroup>
-                                {data.columns.map((col) => (
-                                    <col key={String(col.key)} style={{ width: `${col.width}%` }} />
-                                ))}
-                                <col style={{ width: '63px' }} />
-                            </colgroup>
-                            <thead className="sticky top-0 bg-color-surface-secondary border-solid border-b border-color-border-primary">
-                                <tr className="text-subtitle-1">
-                                    {data.columns.map((col) => (
-                                        <th className={'px-4 py-3 text-left truncate'} key={String(col.key)}>
-                                            {col.label}
-                                        </th>
-                                    ))}
-                                    <th className="px-4 py-3"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.items
-                                    .slice((page - 1) * rowsPerPage, page * rowsPerPage)
-                                    .map((item, index) => (
-                                        <tr key={index} className="text-body-2 border-b border-solid border-color-border-primary hover:bg-color-surface-secondary" onDoubleClick={() => editAction(item)}>
-                                            {data.columns.map((col) => (
-                                                <td className="p-4" key={String(col.key)}>
-                                                    {col.accessor(item)}
-                                                </td>
-                                            ))}
-                                            <td className="p-4 text-center">
-                                                <ActionMenu<T>
-                                                    onEdit={editAction}
-                                                    onDelete={removeAction}
-                                                    item={item}
-                                                />
-                                            </td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                            <tfoot className="sticky bottom-0 bg-color-surface-primary border-solid border-y border-color-border-primary z-10">
-                                <tr className="text-body-2 bg-color-surface-primary">
-                                    <td colSpan={data.columns.length + 1} className="p-4">
-                                        <PaginationRow
-                                            page={page}
-                                            setPage={setPage}
-                                            rowsPerPage={rowsPerPage}
-                                            setRowsPerPage={setRowsPerPage}
-                                            numberOfResults={data.items.length}
-                                        />
-                                    </td>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                )}
-            </ErrorBoundary>
-        </TabPanel>
-    )
-}
+  return (
+    <TabPanel
+      className={
+        'flex-1 flex flex-col items-start self-stretch gap-4 overflow-hidden'
+      }
+    >
+      <ContentTitleBar setQuery={setQueryAndResetPage}>
+        <Button
+          variant={'primary'}
+          icon={Plus}
+          label={'Voeg toe'}
+          onClick={openAddForm}
+        />
+      </ContentTitleBar>
+      <ErrorBoundary
+        fallbackRender={fallbackRender}
+        onReset={errorHandling.reset}
+      >
+        <ErrorThrowingComponent error={errorHandling.error} />
+        {data.items.length === 0 ? (
+          isLoading ? (
+            <div className="flex justify-center items-center h-24 w-full">
+              <ClipLoader
+                size={20}
+                color={'text-color-text-invert-primary'}
+                aria-label="Laad spinner"
+              />
+            </div>
+          ) : renderEmptyState ? (
+            renderEmptyState(openAddForm)
+          ) : null
+        ) : (
+          <div className="flex-1 items-start self-stretch border-t-solid border-t border-t-color-border-primary overflow-y-auto">
+            <table className="w-full table-fixed border-collapse">
+              <colgroup>
+                {data.columns.map((col) => (
+                  <col
+                    key={String(col.key)}
+                    style={{ width: `${col.width}%` }}
+                  />
+                ))}
+                <col style={{ width: '63px' }} />
+              </colgroup>
+              <thead className="sticky top-0 bg-color-surface-secondary border-solid border-b border-color-border-primary">
+                <tr className="text-subtitle-1">
+                  {data.columns.map((col) => (
+                    <th
+                      className={'px-4 py-3 text-left truncate'}
+                      key={String(col.key)}
+                    >
+                      {col.label}
+                    </th>
+                  ))}
+                  <th className="px-4 py-3"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.items
+                  .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+                  .map((item, index) => (
+                    <tr
+                      key={index}
+                      className="text-body-2 border-b border-solid border-color-border-primary hover:bg-color-surface-secondary"
+                      onDoubleClick={() => editAction(item)}
+                    >
+                      {data.columns.map((col) => (
+                        <td className="p-4" key={String(col.key)}>
+                          {col.accessor(item)}
+                        </td>
+                      ))}
+                      <td className="p-4 text-center">
+                        <ActionMenu<T>
+                          onEdit={editAction}
+                          onDelete={removeAction}
+                          item={item}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+              <tfoot className="sticky bottom-0 bg-color-surface-primary border-solid border-y border-color-border-primary z-10">
+                <tr className="text-body-2 bg-color-surface-primary">
+                  <td colSpan={data.columns.length + 1} className="p-4">
+                    <PaginationRow
+                      page={page}
+                      setPage={setPage}
+                      rowsPerPage={rowsPerPage}
+                      setRowsPerPage={setRowsPerPage}
+                      numberOfResults={data.items.length}
+                    />
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        )}
+      </ErrorBoundary>
+    </TabPanel>
+  );
+};
